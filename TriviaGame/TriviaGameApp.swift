2ef8914c27ce129c751024893cfce7a103b87017
stickyfingers489 <mutuadennis56@gmai.com>
2022-05-11 17:22:09 +0300

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by stickerfinger489 on 11/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
